7194e027040e154376f87cae37ed0c2e5309e3e8
Josh Israel <joshisrael@MacBookAir.lan>
2025-10-15 16:23:12 -0700

    fix: remove non-existent function imports

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,12 +1,6 @@
 """Utility functions for the Hume DSPy Agent."""
 
-from .typeform_transform import (
-    transform_typeform_webhook,
-    extract_answer_value,
-    extract_field_name,
-    build_raw_answers,
-    extract_common_fields,
-)
+from .typeform_transform import transform_typeform_webhook
 
 from .security import (
     verify_typeform_signature,
@@ -16,10 +10,6 @@ from .security import (
 __all__ = [
     # Typeform transformation
     "transform_typeform_webhook",
-    "extract_answer_value",
-    "extract_field_name",
-    "build_raw_answers",
-    "extract_common_fields",
     # Security
     "verify_typeform_signature",
     "verify_webhook_signature",
